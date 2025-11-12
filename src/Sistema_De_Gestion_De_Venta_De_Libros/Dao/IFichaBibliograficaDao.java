e7fc6c5d816d980e5b3bf8b0cfd63e53005f4679
Laura-Ambrosio <99255612+Laura-Ambrosio@users.noreply.github.com>
2025-11-12 10:08:12 +0900

    Update and rename IFichaBibliograficaDAO.java to IFichaBibliograficaDao.java
    changed DAO to Dao for consistency

--- src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IFichaBibliograficaDao.java
+++ src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IFichaBibliograficaDao.java
@@ -3,7 +3,7 @@ package sistema.de.gestion.de.venta.de.libros.dao;
 import sistema.de.gestion.de.venta.de.libros.entities.FichaBibliografica;
 import java.sql.Connection;
 
-public interface IFichaBibliograficaDAO extends GenericDao<FichaBibliografica> {
+public interface IFichaBibliograficaDao extends GenericDao<FichaBibliografica> {
 
     
     FichaBibliografica buscarPorISBN(String isbn, Connection conn);
