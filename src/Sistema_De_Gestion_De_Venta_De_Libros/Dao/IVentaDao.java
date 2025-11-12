6989cc1979d75cc9f97b51a2bf75dc67230752df
Laura-Ambrosio <99255612+Laura-Ambrosio@users.noreply.github.com>
2025-11-13 03:43:55 +0900

    Delete src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IVentaDao.java

--- src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IVentaDao.java
+++
@@ -1,8 +0,0 @@
-package sistema.de.gestion.de.venta.de.libros.dao;
-
-import sistema.de.gestion.de.venta.de.libros.entities.Venta;
-
-public interface IVentaDao extends GenericDao<Venta> {
-    
-   
-}
