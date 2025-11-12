c5f20dd010f57bd9ad8ed074e4453908c8d543b3
Laura-Ambrosio <99255612+Laura-Ambrosio@users.noreply.github.com>
2025-11-12 10:07:39 +0900

    Update and rename IClienteDAO.java to IClienteDao.java
    changed DAO to Dao for consistency

--- src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IClienteDao.java
+++ src/Sistema_De_Gestion_De_Venta_De_Libros/Dao/IClienteDao.java
@@ -3,7 +3,7 @@ package sistema.de.gestion.de.venta.de.libros.dao;
 import sistema.de.gestion.de.venta.de.libros.entities.Cliente;
 import java.sql.Connection;
 
-public interface IClienteDAO extends GenericDao<Cliente> {
+public interface IClienteDao extends GenericDao<Cliente> {
 
     
     Cliente buscarPorEmail(String email, Connection conn);
