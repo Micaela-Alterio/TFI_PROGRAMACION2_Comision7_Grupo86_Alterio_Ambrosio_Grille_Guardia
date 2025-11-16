73e7ddd21a17e81f793e7fd17463aafe9ee98cd0
Laura-Ambrosio <99255612+Laura-Ambrosio@users.noreply.github.com>
2025-11-16 23:34:03 +0900

    Update Creacion_de_tablas.sql
    Agregue codificacion utf-8 porque no se veian los acentos, caracteres especiales

--- sql/Creacion_de_tablas.sql
+++ sql/Creacion_de_tablas.sql
@@ -2,7 +2,12 @@
 -- CREACION DE LAS TABLAS
 
 -- Usar la base de datos libreria
-CREATE DATABASE IF NOT EXISTS libreria;
+-- Con codificacion utf-8
+CREATE DATABASE IF EXISTS libreria;
+CREATES libreria;
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
 USE libreria;
 
 -- Tabla: fichas_bibliograficas
@@ -14,7 +19,7 @@ CREATE TABLE fichas_bibliograficas (
     num_paginas INT CHECK (num_paginas > 0),
     sinopsis VARCHAR(2000),
     eliminado TINYINT(1) NOT NULL DEFAULT 0 -- 0: activo, 1: eliminado
-);
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla: libros
 CREATE TABLE libros (
@@ -26,7 +31,7 @@ CREATE TABLE libros (
     genero VARCHAR(50),
     eliminado TINYINT(1) NOT NULL DEFAULT 0,
     FOREIGN KEY (id_ficha) REFERENCES fichas_bibliograficas(id_ficha)
-);
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla: clientes
 CREATE TABLE clientes (
@@ -36,7 +41,7 @@ CREATE TABLE clientes (
     email VARCHAR(100) UNIQUE,
     telefono VARCHAR(20),
     eliminado TINYINT(1) NOT NULL DEFAULT 0
-);
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Tabla: ventas
 CREATE TABLE ventas (
@@ -50,4 +55,4 @@ CREATE TABLE ventas (
     eliminado TINYINT(1) NOT NULL DEFAULT 0,
     FOREIGN KEY (id_libro) REFERENCES libros(id_libro),
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
